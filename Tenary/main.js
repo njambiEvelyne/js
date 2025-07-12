86a086d685a15fceee3f53749ff547ce9a1587ee
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-12 08:44:56 +0300

    Initial commit

---
+++ Tenary/main.js
@@ -0,0 +1,13 @@
+let age = Number(prompt("Enter your age:"));
+let permission = age <18 ?  "Denied" : "Allowed";
+console.log(`You are ${permission} to vote.`);
+
+let ID = true;
+//let Id = Number(prompt("Enter your id number"));
+let group = ID ? "You are an adult" : "You are still a kid";
+console.log(group);
+
+let num = Math.random();
+num = num * 8;
+side = Math.floor(num);
+
