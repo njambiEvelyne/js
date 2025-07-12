474f93dd51df3efef4b82298330eda53277beaed
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-12 16:10:19 +0300

    Added tenarry operators

--- Tenary/main.js
+++ Tenary/main.js
@@ -8,6 +8,22 @@ let group = ID ? "You are an adult" : "You are still a kid";
 console.log(group);
 
 let num = Math.random();
-num = num * 8;
+num = num * 8 ;
 side = Math.floor(num);
+console.log(side);
 
+
+//Rock paper scissors game
+let computer = "scissors";
+let playerOne = "rock";
+let winner = 
+playerOne === computer ? "Tie game!"
+:playerOne === "rock" && computer === "paper"
+? "Computer wins!"
+:playerOne === "paper" && computer === "scissors"
+? "Computer wins!"
+:playerOne === "scissors"  && computer === "rock"
+? "Computer wins!"
+:"Player one wins!"
+
+console.log(winner);
