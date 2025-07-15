d6aca6bd36357ab5aacfe031cbb8f1f3e6d5b582
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-15 09:10:15 +0300

    Added labeled blocks

---
+++ labeled blocks/main.js
@@ -0,0 +1,16 @@
+//Breaking and continue and labeled blocks
+let groups = [
+ ["Martin", "Daniel", "Keith"],
+ ["Margot", "Marina", "Ali"],
+ ["Helen", "Jonah", "Sambikos"],
+];
+outer:
+for (let group of groups) {
+  inner:
+  for (let member of group) {
+    if (member.startsWith("M")){
+      console.log("found one starting with M:", member);
+      continue inner;
+    }
+  }
+}
