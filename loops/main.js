4c14aeb5be54fdce9e15a113089e936dffc3da4c
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-15 08:59:57 +0300

    Specified the search items

--- loops/main.js
+++ loops/main.js
@@ -56,3 +56,63 @@ console.table(arrayOfArrays);
 //   names = "Hello " + names;
 //   console.log(names);
 // }
+
+
+let myCar = {
+  make : "Volkswagen",
+  model : "Golf",
+  year : "1999",
+  color : "black",
+}
+for (let prop in myCar){
+  let values = (myCar[prop]);
+  console.log(values);
+  //console.log(prop);
+}
+//This helps in the convertion of the keys and thevalues of the object into an array 
+let cars = Object.keys(myCar);
+console.log(cars);
+
+let car2 = Object.values(myCar);
+console.log(car2);
+  
+let arrKeys = Object.keys(myCar);
+for(let i = 0; i < arrKeys.length; i++) {
+ console.log(arrKeys[i] + ": " + myCar[arrKeys[i]]);
+}
+
+let vehicles = [
+ {
+ model: "Golf",
+ make: "Volkswagen",
+ year: 1999,
+ color: "black",
+ },
+ {
+ model: "Picanto",
+ make: "Kia",
+ year: 2020,
+ color: "red",
+ },
+ {
+ model: "Peugeot",
+ make: "208",
+ year: 2021,
+ color: "black",
+ },
+ {
+ model: "Fiat",
+ make: "Punto",
+ year: 2020,
+ color: "black",
+ }
+];
+
+for (let i = 0; i < vehicles.length; i++) {
+  if (vehicles[i].year >= 2021) {
+    if (vehicles[i].color === "black") {
+    console.log("I have found my new car:", vehicles[i]);
+    break;
+    }
+  }
+}
