c363a6ed213b22357e48373fdc21237ed0c35ba4
njambiEvelyne <evelynenganga76@gmail.com>
2025-07-28 21:24:50 +0300

    Added a new file

---
+++ DragDropElements/main.js
@@ -0,0 +1,20 @@
+// const elm1 = document.querySelector(".output");
+// console.log(elm1);
+
+// const mainList = document.querySelector("#mainlist");
+// console.log(mainList);
+
+// console.log("Query selector");
+// const elem = document.querySelector("div");
+// console.log(elem);
+// document.getElementById("clickHandler").onclick = function (){
+//     alert("Auch!Stop!");}
+
+let container = document.getElementsByClassName("container");
+console.log(container);
+
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+
+
+
