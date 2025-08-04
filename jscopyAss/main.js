53f2fe00028bac8151e04c01ebe7c69118db3714
njambiEvelyne <evelynenganga76@gmail.com>
2025-08-04 16:46:43 +0300

    Added new folder

---
+++ jscopyAss/main.js
@@ -0,0 +1,23 @@
+//Logis Statements
+alert("Welcome to the program");
+let rain = true;
+if(rain){
+ console.log("** Taking my umbrella when I need to go outside **");
+} else {
+ console.log("** I can leave my umbrella at home **");
+}
+
+let studying = true;
+if (studying){
+  console.log("I love studying.");
+}else{
+  console.log("*** Can you teach me that ***");
+}
+
+let hobby = "coding";
+if (hobby){
+  console.log("I love the coding tool");
+}
+if (!hobby){
+  console.log("What do you love doing?");
+}
